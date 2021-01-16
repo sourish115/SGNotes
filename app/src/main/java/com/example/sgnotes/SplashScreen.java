898ac308d6b979ca912be89ada04b0fb63efb787
sourish ghosh <sourish115@live.com>
2021-01-16 17:18:43 +0530

    Added code for Login and register using FirebaseAuth

--- app/src/main/java/com/example/sgnotes/SplashScreen.java
+++ app/src/main/java/com/example/sgnotes/SplashScreen.java
@@ -1,14 +1,58 @@
 package com.example.sgnotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashScreen extends AppCompatActivity {
-
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        fAuth = FirebaseAuth.getInstance();
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                if(fAuth.getCurrentUser() != null){
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    finish();
+                }else {
+
+                    fAuth.signInAnonymously().addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Toast.makeText(SplashScreen.this, "Logged in With Temporary Account.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            finish();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(SplashScreen.this, "Error ! " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    });
+                }
+
+
+
+
+            }
+        },2000);
     }
 }
