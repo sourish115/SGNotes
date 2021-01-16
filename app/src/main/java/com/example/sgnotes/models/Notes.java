ed83f6352946da97196f3b435f626b26be91898a
sourish ghosh <sourish115@live.com>
2021-01-16 17:09:42 +0530

    added menus and started MainActivity.java

---
+++ app/src/main/java/com/example/sgnotes/models/Notes.java
@@ -0,0 +1,28 @@
+package com.example.sgnotes.models;
+
+public class Notes {
+    private String title;
+    private String content;
+
+    public Notes(){}
+    public Notes(String title,String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
