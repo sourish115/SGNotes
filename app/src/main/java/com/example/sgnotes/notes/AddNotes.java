1a66a2ee9e1ffa240dcc0092ffdff1e1462ac9a3
sourish ghosh <sourish115@live.com>
2021-01-16 16:20:42 +0530

    First Commit

---
+++ app/src/main/java/com/example/sgnotes/notes/AddNotes.java
@@ -0,0 +1,16 @@
+package com.example.sgnotes.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sgnotes.R;
+
+public class AddNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_notes);
+    }
+}
---
+++ app/src/main/java/com/example/sgnotes/notes/EditNotes.java
@@ -0,0 +1,16 @@
+package com.example.sgnotes.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sgnotes.R;
+
+public class EditNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_notes);
+    }
+}
